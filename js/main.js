a5e9d10eda65a3b7f82fc7b0536103fa7a13e2c8
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-11-20 21:46:25 +0400

    !

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const userEdit = document.querySelector(".edit")
 const userDelete = document.querySelector(".delete")
 const userId = document.querySelector(".id")
 const userName = document.querySelector(".name")
-const Email = document.querySelector(".email")
+const userEmail = document.querySelector(".email")
 const userPersonalId = document.querySelector(".personal-id")
 const userMobNumber = document.querySelector(".mobile-number")
 const userZipCode = document.querySelector(".zip")
@@ -17,6 +17,13 @@ const userGender = document.querySelector(".gender")
 const registration = document.querySelector(".registration")
 const closingModal = document.querySelector('#close')
 const formInfo = document.querySelector('.add-form')   
+const newUserName = document.querySelector("#new-user")
+const newUserEmail = document.querySelector(".new-user-email")
+const newUserPersonalId = document.querySelector("#new-user-personal-id")
+const newUserMobNumber = document.querySelector("#new-user-mob-number")
+const newUserZipCode = document.querySelector("#new-user-zip-code")
+const newUserGenderfemale = document.querySelector("#gender-female")
+const newUserGenderMale = document.querySelector("#gender-male")
 
 
 function getAllUser() {
@@ -28,7 +35,7 @@ function getAllUser() {
     console.log(data)
     userId.innerHTML = users[3].id
     userName.innerHTML = users[3].first_name
-    Email.innerHTML = users[3].email
+    userEmail.innerHTML = users[3].email
     userPersonalId.innerHTML = users[3].id_number
     userMobNumber.innerHTML = users[3].phone
     userZipCode.innerHTML = users[3].zip_code
@@ -57,8 +64,48 @@ function startModal() {
 startModal()
 
 
+formInfo.addEventListener("submit",(e)=>{
+  e.preventDefault()
+
+  const newUserNameValue = newUserName.value
+  const newUserEmailValue = newUserEmail.value
+  const newUserPersonalIdValue = newUserPersonalId.value
+  const newUserMobNumberValue = newUserMobNumber.value
+  const newUserZipCodeValue = newUserZipCode.value
+  const newUserGenderfemaleValue = newUserGenderfemale.value
+  const newUserGendermaleValue = newUserGenderMale.value
 
 
+  
+    const user = {
+      first_name: newUserNameValue,
+      email: newUserEmailValue,
+      id_number: newUserPersonalIdValue,
+      phone: newUserMobNumberValue,
+      zip_code: newUserZipCodeValue,
+      gender: newUserGenderfemaleValue,newUserGendermaleValue
+    }  
+    addNewUser(user)
+    formInfo.reset()
+})
+
+function addNewUser(userInfo){
+  fetch("https://borjomi.loremipsum.ge/api/register",{
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(userInfo)  
+  })
+  .then((res)=> res.json())
+  .then((data)=> {
+    
+  })
+  .catch(err=>{
+  })
+}
+
+userEdit.addEventListener("click",()=>{
+  
+})
 
 
 
